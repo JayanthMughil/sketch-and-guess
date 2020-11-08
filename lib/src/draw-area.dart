b2ff948d8c54855a59643351c22520b992e14b97
Jayanthanan abaian <jayanthananabaian@Jayanthanans-MacBook-Pro.local>
2020-11-08 18:34:03 +0530

    feature: canvas brush size

--- lib/src/draw-area.dart
+++ lib/src/draw-area.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:menu_button/menu_button.dart';
 
 class DrawArea extends StatefulWidget {
-
-  const DrawArea({Key key}): super(key: key);
+  const DrawArea({Key key}) : super(key: key);
 
   @override
   _DrawArea createState() => _DrawArea();
-
 }
 
 class _DrawArea extends State<DrawArea> {
 
   List<Offset> _points = <Offset>[];
+  List<double> brushSizes = [5.0, 10.0, 15.0, 20.0, 25.0];
+  double brushSize = 10.0;
+  List<CanvasPainter> brushList = [];
 
   void trackPath(DragUpdateDetails details) {
     setState(() {
@@ -23,73 +25,167 @@ class _DrawArea extends State<DrawArea> {
 
   @override
   Widget build(BuildContext context) {
-    return
-      Container(
-          margin: EdgeInsets.all(10.0),
-          height: 370,
-          decoration: BoxDecoration(
-            border: Border.all(width: 2.0, color: Colors.black),
-            borderRadius: BorderRadius.circular(5.0)
-          ),
-          child: Column(
-            children: [
-              Container(
-                height: 55,
-                decoration: BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(width: 2.0, color: Colors.black)
-                  )
-                ),
-                child: Row(
-                  children: [],
-                ),
-              ),
-              Expanded(
-                child: LayoutBuilder(
-                  builder: (context, constraints) {
-                    return Container(
-                      width: constraints.widthConstraints().maxWidth,
-                      height: constraints.heightConstraints().maxHeight,
-                      child: GestureDetector(
-                          onPanUpdate: trackPath,
-                          onPanEnd: (DragEndDetails details) => _points.add(null) ,
-                          child: ClipRect(
-                            child: CustomPaint(
-                              painter: CanvasPainter(_points),
-                            )
-                          )
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      height: 420,
+      decoration: BoxDecoration(
+          border: Border.all(width: 2.0, color: Colors.black),
+          borderRadius: BorderRadius.circular(5.0)),
+      child: Column(
+        children: [
+          Container(
+            height: 55,
+            decoration: BoxDecoration(
+                border: Border(
+                    bottom: BorderSide(width: 2.0, color: Colors.black))),
+            child: Material(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                      child: LayoutBuilder (
+                        builder: (context, constraints) {
+                          return Container (
+                              alignment: Alignment.center,
+                              child: MenuButton(
+                                child: Container (
+                                  width: constraints.widthConstraints().maxWidth,
+                                  height: constraints.heightConstraints().maxHeight,
+                                  alignment: Alignment.center,
+                                  child: Icon(
+                                    Icons.brush,
+                                    size: 30,
+                                  ),
+                                ),//
+                                scrollPhysics: AlwaysScrollableScrollPhysics(),
+                                topDivider: true,// Widget displayed as the button
+                                items: brushSizes,// List of your items
+                                itemBuilder: (value) => Container(
+                                    width: constraints.heightConstraints().maxWidth,
+                                    height: constraints.heightConstraints().maxHeight,
+                                    color: brushSize == value ? Colors.grey[300] : Colors.white,
+                                    alignment: Alignment.center,
+                                    child: Icon(
+                                      Icons.circle,
+                                      size: value,
+                                    ),
+                                ),// Widget dis
+                                divider: Container(
+                                  height: 0,
+                                  color: Colors.grey,
+                                ),// played for each item
+                                toggledChild: Container(
+                                  color: Colors.white,
+                                  child: Container (
+                                    width: constraints.widthConstraints().maxWidth,
+                                    height: constraints.heightConstraints().maxHeight,
+                                    alignment: Alignment.center,
+                                    child: Icon(
+                                      Icons.brush,
+                                      size: 30,
+                                    ),
+                                  ),// Widget displayed as the button,
+                                ),
+                                onItemSelected: (value) {
+                                  setState(() {
+                                    List<Offset> oldPoints = List.from(_points);
+                                    brushList.add(CanvasPainter(points: oldPoints, brushSize: brushSize));
+                                    brushSize = value;
+                                    _points.clear();
+                                  });
+                                  // Action when new item is selected
+                                },
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.grey[300]),
+                                    borderRadius: const BorderRadius.all(Radius.circular(3.0)),
+                                    color: Colors.white
+                                ),
+                                onMenuButtonToggle: (isToggle) {
+
+                                },
+                              )
+                          );
+                        },
+                      )
+                  ),
+                  Container(
+                    width: 120,
+                    decoration: BoxDecoration(
+                        border: Border(
+                            right: BorderSide(width: 2.0, color: Colors.black),
+                            left: BorderSide(width: 2.0, color: Colors.black))),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.color_lens,
+                        size: 30,
                       ),
-                    );
-                  },
-                )
+                      onPressed: null,
+                    ),
+                  ),
+                  Expanded(
+                      child: Container(
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.format_color_fill,
+                        size: 30,
+                      ),
+                      onPressed: null,
+                    ),
+                  ))
+                ],
               )
-            ],
+            ),
           ),
-        );
+          Expanded(child: LayoutBuilder(
+            builder: (context, constraints) {
+              return Container(
+                width: constraints.widthConstraints().maxWidth,
+                height: constraints.heightConstraints().maxHeight,
+                child: GestureDetector(
+                    onPanUpdate: trackPath,
+                    onPanEnd: (DragEndDetails details) => _points.add(null),
+                      child: ClipRect(
+                          child: CustomPaint(
+                        painter: CanvasPainter(points: _points, brushSize: brushSize, brushList: brushList),
+                      )
+                    )
+                ),
+              );
+            },
+          ))
+        ],
+      ),
+    );
   }
 }
 
 class CanvasPainter extends CustomPainter {
-
   List<Offset> points;
+  double brushSize;
+  List<CanvasPainter> brushList;
 
-  CanvasPainter(this.points);
-
-  Paint brush = Paint()
-  ..color = Colors.black
-  ..strokeCap = StrokeCap.round
-  ..strokeWidth = 10.0;
+  CanvasPainter({this.points, this.brushSize, this.brushList = const[]});
 
   @override
   void paint(Canvas canvas, Size size) {
-    for (int i = 0; i < points.length-1; i++) {
-      if (points[i] != null && points[i+1] != null) {
-        canvas.drawLine(points[i], points[i+1], brush);
+    
+    for (CanvasPainter painter in brushList) {
+      painter.paint(canvas, size);
+    }
+
+    Paint brush = Paint()
+      ..color = Colors.black
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = brushSize;
+
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null) {
+        canvas.drawLine(points[i], points[i + 1], brush);
       }
     }
+
   }
 
   @override
   bool shouldRepaint(CanvasPainter oldDelegate) => oldDelegate.points != points;
-
 }
